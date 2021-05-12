f46bee52a3c06c72bd710a6d4b2d4882cbf2053a
jqzhuuuu <joshzhu12@gmail.com>
2021-05-11 22:12:44 -0700

    add lol.js

---
+++ lol.js
@@ -0,0 +1,9 @@
+/**
+ * lol test function haha
+ * @param {number} p1 the first num
+ * @param {number} p2 the second num
+ * @returns {number} output lol
+*/
+function myFunction(p1, p2) {
+	  return p1 * p2;   // The function returns the product of p1 and p2
+}
