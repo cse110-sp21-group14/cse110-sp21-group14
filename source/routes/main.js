0369fb1e2e32b0b66f21177fdacb522988b4b545
Stephen Montes De Oca <deocastephen@gmail.com>
2021-05-16 20:39:31 -0700

    finished all fetch requests

--- source/routes/main.js
+++ source/routes/main.js
@@ -85,4 +85,64 @@ router.get('/:id', ensureAuth, async (req, res) => {
     }
 });
 
+// @desc gets the pages of a specific journal
+// @route GET
+router.get('/:id/pages', ensureAuth, async (req,res) => {
+    try {
+        // getting journal to get pages
+        let encryptedJournal = await Journal.findOne({ googleId: req.user.googleId, _id: req.params.id }).lean();
+        let pageIds = encryptedJournal.pageIds;
+        let decryptedPages = [];
+
+        // iterating over each page and decrypting them
+        for (let i = 0; i < pageIds.length; i++) {
+            // getting page from pageId
+            let encryptedPage = await Page.findOne({ googleId: req.user.googleId, _id: pageIds[i] }).lean();
+            let decryptedPage = {
+                _id: pageIds[i],
+                title: secret.decrypt(encryptedPage.title),
+                googleId: req.user.googleId,
+                journalId: req.params.id,
+                lastModified: encryptedPage.lastModified,
+                importance: encryptedPage.importance,
+                content: secret.decrypt(encryptedPage.content)
+            };
+
+            decryptedPages.push(decryptedPage);
+        }
+
+        // responding with a list of pages
+        res.json(decryptedPages);
+    } catch (err) {
+        console.log(err);
+        // TODO: determine what to return from a fetch that results in an error
+    }
+});
+
+// @desc Get a specific page from a specific journal
+// @route GET
+router.get('/:id/:id', ensureAuth, async (req, res) => {
+    try {
+        // getting page from id
+        let encryptedPage = await Page.findOne({ googleId: req.user.googleId, _id: req.params.id }).lean();
+        
+        // decrypting page
+        let decryptedPage = {
+            _id: req.params.id,
+            title: secret.decrypt(encryptedPage.title),
+            googleId: req.user.googleId,
+            journalId: encryptedPage.journalId,
+            lastModified: encryptedPage.lastModified,
+            importance: encryptedPage.importance,
+            content: secret.decrypt(encryptedPage.content)
+        };
+
+        // responding with the requested page
+        res.json(decryptedPage);
+    } catch (err) {
+        console.log(err);
+        // TODO: determine what to return from a fetch that results in an error
+    }
+});
+
 module.exports = router;
