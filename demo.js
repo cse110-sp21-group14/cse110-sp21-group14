8c119eacd20cee57edaed18c401b192800f771c6
jqzhuuuu <joshzhu12@gmail.com>
2021-05-17 03:33:06 -0700

    add demo.js

---
+++ demo.js
@@ -0,0 +1,12 @@
+/**
+ * test function for JSDoc creation
+ * This function multiplies two parameters p1 and p2 and returns the result
+ * @param {number} p1 the first num
+ * @param {number} p2 the second num
+ * @returns {number} output lol
+*/
+function myFunction(p1, p2) {
+    return p1 * p2;    // The function returns the product of p1 and p2
+}
+
+myFunction(1,2);
