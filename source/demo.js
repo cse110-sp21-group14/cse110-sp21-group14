0394f06b84a3dfa50c437aa53c01632366cc7b7c
nwong524 <60756614+nwong524@users.noreply.github.com>
2021-05-25 20:44:05 -0700

    added demo.js

---
+++ source/demo.js
@@ -0,0 +1,11 @@
+/**
+ * Test function for JSDoc creation
+ * This fuction mulitplies two parameters a and b and returns the result.
+ * @param {number} a 
+ * @param {number} b 
+ * @returns The product of a and b
+ */
+ function demoFunction(a, b) {
+    let c = 'test string'
+    return a * b;    // The function returns the product of a and b
+}
