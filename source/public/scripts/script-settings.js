16db9918f8d340c2b4051cf23c79611f6868ae97
Stephen Montes De Oca <deocastephen@gmail.com>
2021-06-03 19:36:24 -0700

    disabled settings.js

--- source/public/scripts/script-settings.js
+++ source/public/scripts/script-settings.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 function changeFont(selectTag) {
     var listValue = selectTag.options[selectTag.selectedIndex].text;
     document.getElementById("txt").style.fontSize = listValue;
