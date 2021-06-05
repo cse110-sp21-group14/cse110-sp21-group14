81ce6ccf4ee35b712d72bcb4d8bf509af384da60
Joshua Zhu <62978132+jqzhuuuu@users.noreply.github.com>
2021-06-04 17:50:55 -0700

    Update script-settings.js

--- source/public/scripts/script-settings.js
+++ source/public/scripts/script-settings.js
@@ -6,13 +6,13 @@
 function changeFont(selectTag) {
     var listValue = selectTag.options[selectTag.selectedIndex].text;
     document.getElementById("txt").style.fontSize = listValue;
-  }
+}
 
 /**
  * Changes the background of the app
  * @function
  */
 function toggledarkmode() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
+    var element = document.body;
+    element.classList.toggle("dark-mode");
 }
