a4270d308bb859292a7536fbcb6c80b98677b101
Aidan Liu <lyz784125253@gmail.com>
2021-06-06 19:46:20 -0700

    nav bar font

--- source/public/scripts/script-settings.js
+++ source/public/scripts/script-settings.js
@@ -11,9 +11,17 @@ function closeSettings(){
 function changeFont(font){
     document.getElementById("popupTitle").style.fontFamily = font.value;
     document.getElementById("contains").style.fontFamily = font.value;
+    document.getElementById("journal").style.fontFamily = font.value;
+    document.getElementById("mycalendar").style.fontFamily = font.value;
+    document.getElementById("mysettings").style.fontFamily = font.value;
+    document.getElementById("signout").style.fontFamily = font.value;
 }
 
 function toggleDarkMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
+  element.classList.toggle("mycalendar");
+  element.classList.toggle("signout");
+  element.classList.toggle("mysettings");
+  element.classList.toggle("journal");
 }
