5b6f1505fb83c2e972ce97ec5e45b6801f577b72
Michael Mao <michaelmao1999@gmail.com>
2021-06-07 19:40:32 -0700

    Finished Calendar Tests

--- source/testing/script-calendar.test.js
+++ source/testing/script-calendar.test.js
@@ -3,15 +3,17 @@
  * @author Michael Mao
  */
 
+//Constant variables used for testing
 const GOOGLE_USER = 'PuffNotesTester';
 const GOOGLE_PWD = 'PowellPuffGang14';
 const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const URL = "http://localhost:5000/main/daily/60bc1482340ef13674a421f6";
 
 //Puppeteer E2E tests for calendar webpage
 describe("Testing Calendar Webpage", () => {
     //Logs into Puff Notes and navigates to Calendar webpage.
     beforeAll(async () => {
-        
+
         //Opens up website
         await page.goto("http://localhost:5000/");
         await page.waitForTimeout(500);
@@ -34,18 +36,91 @@ describe("Testing Calendar Webpage", () => {
         await page.waitForSelector('body > div > div.menuBar > ul > li:nth-child(2) > a');
         await page.click('body > div > div.menuBar > ul > li:nth-child(2) > a');
         await navigationPromise;
-        await page.waitForTimeout(500);
-    }, (10000));
+    }, 30000);
 
     it("Check if default month is the current month", async () => {
         const curDate = new Date();
         const curMonth = curDate.getMonth();
         const curYear = curDate.getFullYear();
         const expectedDate = MONTHS[curMonth] + ", " + curYear;
+        await page.waitForSelector(".today");
+        const pageDate = await page.$eval(".today", element => {
+            return element.innerHTML;
+        });
+        expect(pageDate).toBe(expectedDate);
+    }, 10000);
+
+    it("Check if previous month button works", async () => {
+        const curDate = new Date();
+        const curMonth = curDate.getMonth() - 1;
+        const curYear = curDate.getFullYear();
+        const expectedDate = MONTHS[curMonth] + ", " + curYear;
+
+        //Click previous month button
+        const button = await page.$("#calendar > div > div > div.cld-rwd.cld-nav");
+        await button.evaluate(button => button.click());
+        await page.waitForTimeout(1000);
         const pageDate = await page.$eval(".today", (element) => {
             return element.innerHTML;
         });
         expect(pageDate).toBe(expectedDate);
-    });
+    }, 10000);
+
+    it("Check if next month button works", async () => {
+        const curDate = new Date();
+        const curMonth = curDate.getMonth();
+        const curYear = curDate.getFullYear();
+        const expectedDate = MONTHS[curMonth] + ", " + curYear;
+        await page.waitForTimeout(500);
+
+        //Click next month button
+        await page.evaluate(() => {
+            document.querySelector("#calendar > div > div > div.cld-fwd.cld-nav").click();
+        });
+        await page.waitForTimeout(1000);
+        const pageDate = await page.$eval(".today", (element) => {
+            return element.innerHTML;
+        });
+        expect(pageDate).toBe(expectedDate);
+    }, 10000);
+
+    it("Check if daily title is correct", async () => {
+        const curDate = new Date();
+        const desiredDate = new Date(2021, 5);
+
+        //If current month is before the test daily
+        while (desiredDate.getFullYear() > curDate.getFullYear() || (desiredDate.getFullYear() == curDate.getFullYear() && desiredDate.getMonth() > curDate.getMonth())) {
+            await page.evaluate(() => {
+                document.querySelector("#calendar > div > div > div.cld-fwd.cld-nav").click();
+            });
+            await page.waitForTimeout(500);
+            curDate.setMonth(curDate.getMonth() + 1);
+        }
+
+        //If the current month is after the test daily
+        while (desiredDate.getFullYear() < curDate.getFullYear() || (desiredDate.getFullYear() == curDate.getFullYear() && desiredDate.getMonth() < curDate.getMonth())) {
+            await page.evaluate(() => {
+                document.querySelector("#calendar > div > div > div.cld-rwd.cld-nav").click();
+            });
+            await page.waitForTimeout(500);
+            curDate.setMonth(curDate.getMonth() - 1);
+        }
+
+        //Test the title of the daily
+        await page.waitForSelector("#calendar > div > ul.cld-days > li.cld-day.currMonth.eventday > p > span > a");
+        const dailyBox = await page.$("#calendar > div > ul.cld-days > li.cld-day.currMonth.eventday > p > span > a");
+        const dailyTitle = await dailyBox.evaluate(button => button.innerHTML);
+        expect(dailyTitle).toBe("Daily for June 3");
+    }, 20000);
+
+    it("Check if clicking a date opens the daily", async () => {
+        await page.waitForSelector("#calendar > div > ul.cld-days > li.cld-day.currMonth.eventday > p > span > a");
+        const button = await page.$("#calendar > div > ul.cld-days > li.cld-day.currMonth.eventday > p > span > a");
+        await button.evaluate(button => button.click());
+        await page.waitForTimeout(500);
+        await page.waitForSelector("body > div.container > div.viewarea > h1");
+        const dailyUrl = await page.url();
+        expect(dailyUrl).toBe(URL);
+    }, 10000);
 
 });
